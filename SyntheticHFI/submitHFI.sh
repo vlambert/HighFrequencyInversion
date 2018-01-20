809b334ba6235f6d3a6360dafdadeda119f88cd3
Valere Lambert <valerelambert@gmail.com>
2018-01-20 08:49:29 -0800

    synthetic submit file

--- SyntheticHFI/submitHFI.sh
+++ SyntheticHFI/submitHFI.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- matlab -nodisplay -nosplash -nodesktop -r "try, run('MultiArray_HFI_Optimize.m'), catch, exit, end, exit;"
+ matlab -nodisplay -nosplash -nodesktop -r "try, run('MultiFreq_Complex.m'), catch err, fid=fopen('errorFile','w+'),fprintf(fid,'%s',err.getReport('extended','hyperlinks','off')),fclose(fid), exit, end, exit;"
 echo "matlab exit code: $?"
